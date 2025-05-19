795ef7f45950c9fa87784cfd556f3eb97c0bed0e
baynhamch <130159962+baynhamch@users.noreply.github.com>
2025-05-19 18:17:20 -0400

    Initial Commit

---
+++ Algo/AlgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoApp.swift
+//  Algo
+//
+//  Created by Nicholas Conant-Hiley on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AlgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
